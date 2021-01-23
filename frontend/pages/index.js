ef1cf57d86e6074bcbbae8f2885bd2d72438a13e
Jairo Junior <jairojunior.dev@gmail.com>
2021-01-23 01:23:07 -0300

    fazendo a tela de login

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import 'animate.css'
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        olá mundo
+      </h1>
+    </main>
+  )
+}
